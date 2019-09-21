37d6a1c6bc9a9a5b5819a79495864e1b2c645358
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-21 15:03:04 +0900

    implement view

--- app/src/main/java/jp/cordea/eyes/MainActivity.kt
+++ app/src/main/java/jp/cordea/eyes/MainActivity.kt
@@ -1,12 +1,23 @@
 package jp.cordea.eyes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity(), MainContract.View {
+    @Inject
+    lateinit var presenter: MainContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.destroy()
+    }
+
+    override fun updateText(text: String) {
     }
 }
