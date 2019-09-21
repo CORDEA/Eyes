d55f3858ff84b7d69a2ebae85a9f5a775b0677f0
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-21 12:03:12 +0900

    provide retrofit

--- app/src/main/java/jp/cordea/eyes/AppModule.kt
+++ app/src/main/java/jp/cordea/eyes/AppModule.kt
@@ -1,6 +1,27 @@
 package jp.cordea.eyes
 
 import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 @Module
-interface AppModule
+class AppModule {
+    @Provides
+    fun provideRetrofit(client: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create())
+            .baseUrl(BuildConfig.API_BASE_URL)
+            .client(client)
+            .build()
+
+    @Provides
+    fun provideOkHttp(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BASIC
+            })
+            .build()
+}
