b9d5dd22331fd55bd568a33ca32a58127498db24
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-21 14:28:19 +0900

    install timber

--- app/src/main/java/jp/cordea/eyes/App.kt
+++ app/src/main/java/jp/cordea/eyes/App.kt
@@ -2,8 +2,16 @@ package jp.cordea.eyes
 
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import timber.log.Timber
 
 class App : DaggerApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
         DaggerAppComponent.factory().create(this)
 }
